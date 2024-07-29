11b837ce07ae532ee444e48a1379f95a193aae1e
mikung-invitrace <teerawat.jo@invitracehealth.com>
2024-07-29 19:29:34 +0700

    git actions

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="text-center mt-10">
-      test nextjs mikung
+      test nextjs mikung 555 in ratchaburi
     </div>
   );
 }
